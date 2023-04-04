68cd9c705c16a910445183d7333b975ee6a6fdce
Liam <liamkend@umich.edu>
2023-04-04 13:36:09 -0400

    basic get_weather

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-import os
+import json
 import unittest
+import os
 
+def get_weather(city, date):
+    baseURL = 'http://api.weatherstack.com/'
+    key = '6065327eb56f1efe78274c0de25adead'
+    try:
+        r = requests.get(baseURL + 'current?access_key=' + key + '&query=' + city + '&historical_date=' + date)
+        return json.loads(r.text)
+    except:
+        print('Error: Could not get request')
+        return None
 
-if __name__ == '__main__':
+class TestHomework6(unittest.TestCase):
+    def test_get_weather(self):
+        weather = get_weather('New York', '2015-21-01')
+        print(weather)
+    
+if __name__ == "__main__":
     unittest.main(verbosity=2)
