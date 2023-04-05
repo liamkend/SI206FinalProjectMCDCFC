58407e8bd8e50997a271f97dc18a9929e86b5e5c
Liam <liamkend@umich.edu>
2023-04-05 16:53:38 -0400

    removed tests

--- main.py
+++ main.py
@@ -28,7 +28,8 @@ def get_weather_data(city, month, day, year):
 # need to format the dictionary to put in files
 def create_game_files(gamesDict):
     for game in gamesDict:
-        filename = game
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        filename = dir_path + '/' + game + '.json'
 
         city = game[0]
         month = re.search('^(\d{2})-', game[1])
@@ -39,21 +40,8 @@ def create_game_files(gamesDict):
         write_json(filename, weather)
 
 class TestHomework6(unittest.TestCase):
-    def setUp(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        self.filename = dir_path + '/' + "weather_api.json"
-        self.cache = load_json(self.filename)
-
-    def test_write_json(self):
-        write_json(self.filename, self.cache)
-        dict1 = load_json(self.filename)
-        self.assertEqual(dict1, self.cache)
-
-    def test_get_weather(self):
-        newyork = get_weather_data('New York', '01', '21', '2015')
-        self.assertEqual(type(newyork), dict)
-        self.assertEqual(get_weather_data('Detroit', '13', '20', '2020'), None)
-        print(newyork)
+    def test():
+        pass
     
 if __name__ == "__main__":
     unittest.main(verbosity=2)
