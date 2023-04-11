a6a424e6c9d990d54dc9e0917482210bb8954020
zeisman <zeisman@umich.edu>
2023-04-11 19:40:15 -0400

    created Zack's scraping file for FB ref. Will start scraping in next couple of days.

---
+++ ProFBref_scraping.py
@@ -0,0 +1,9 @@
+#PYTHON FILE FOR SCRAPING PRO FB REFERENCE
+
+first_pfb_ref_url = 'https://www.pro-football-reference.com/years/2022/week_1.htm'
+
+#STEPS
+#For each game on this page... go to the games page
+#Collect all necessary info from the game's page and store in the dict
+#Same for each game on this page... collect link to next page
+#Go to the next page
